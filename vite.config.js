8ef1e63a88489bf7f1dc007f72c9cccdab3cf97a
ananyar <ananyarananyar8@gmail.com>
2025-01-20 17:37:09 +0530

    Changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-// // vite.config.mjs
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
+// vite.config.mjs
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', 
-    port: 3000
-  }
+  plugins: [react()],
 });
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// export default defineConfig({
+//   server: {
+//     host: '0.0.0.0', 
+//     port: 3000
+//   }
+// });
